4e5fdda99b456e6b5686b959bb546efafd8c4918
Ayushijagg <st10452981@vcconnect.edu.za>
2026-07-27 16:51:02 +0200

    Add navigation-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.navigation.compose)
 }
