3231e74723f28da04717b41c20e11695d242fa91
ST10288433 <126710590+ST10288433@users.noreply.github.com>
2026-07-29 00:14:20 +0200

    Updated the colour palette to match the Login and Register screens

--- app/src/main/java/com/ayushi/will/ui/theme/Color.kt
+++ app/src/main/java/com/ayushi/will/ui/theme/Color.kt
@@ -2,10 +2,25 @@ package com.ayushi.will.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val KksRed = Color(0xFFA6332B)
+val KksRedDark = Color(0xFF7E241E)
+val KksRedLight = Color(0xFFC24940)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val KksCreamBg = Color(0xFFF5EFE6)
+val KksWhite = Color(0xFFFFFFFF)
+
+val KksTextPrimary = Color(0xFF2B2320)
+val KksTextSecondary = Color(0xFF8A7F76)
+val KksHint = Color(0xFFB5AAA0)
+
+val KksCardStroke = Color(0xFFEAE0D5)
+val KksStar = Color(0xFFE8A93A)
+
+val KksSuccess = Color(0xFF3F8F5F)
+val KksError = Color(0xFFB3261E)
+
+// Dark mode
+val KksBgDark = Color(0xFF1B1614)
+val KksSurfaceDark = Color(0xFF2A2320)
+val KksTextPrimaryDark = Color(0xFFF2EAE3)
+val KksTextSecondaryDark = Color(0xFFB7ACA3)
